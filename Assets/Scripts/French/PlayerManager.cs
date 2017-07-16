90346034ed661d7f0ebacbe959000dfbf466d533
Andrea Franciosi <french_04@hotmail.it>
2017-07-16 16:07:44 +0200

    modifiche selezione personaggi scena french

--- Assets/Scripts/French/PlayerManager.cs
+++ Assets/Scripts/French/PlayerManager.cs
@@ -4,40 +4,60 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-
-
-    bool isMove = false, isActions = false, isWhait = false;
-    bool isSelected;
     RaycastHit cameraRay;
-    public Material selectedMaterial;
-    public Camera camera;
+    public Material selectedMaterial, p1Material, p2Material;
+    bool p1isMove, p1isActions, p1isWhait, p1isSelected;
+    bool p2isMove, p2isActions, p2isWhait, p2isSelected;
+    public GameObject player1, player2;
+    //public Camera camera;
 
     // Use this for initialization
     void Start()
     {
-
+        p1isMove = GetComponent<Player1>().isMove;
+        p1isActions = GetComponent<Player1>().isActions;
+        p1isWhait = GetComponent<Player1>().isWhait;
+        p1isSelected = GetComponent<Player1>().isSelected;
+        p2isMove = GetComponent<Player2>().isMove;
+        p2isActions = GetComponent<Player2>().isActions;
+        p2isWhait = GetComponent<Player2>().isWhait;
+        p2isSelected = GetComponent<Player2>().isSelected;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out cameraRay);
+        Physics.Raycast(gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition), out cameraRay);
 
-        if (cameraRay.collider != null && Input.GetMouseButtonDown(0)  && cameraRay.transform.tag == "Player")
+        if (cameraRay.collider != null && Input.GetMouseButtonDown(0)  && cameraRay.transform.tag == "Player1")
         {
-            isSelected = true;
-            this.GetComponent<Renderer>().material = selectedMaterial;
+            p1isSelected = true;
+            p2isSelected = false;
+            player1.GetComponent<Renderer>().material = selectedMaterial;
+            player2.GetComponent<Renderer>().material = p2Material;
         }
 
-        else
+        //else
 
-        if (cameraRay.collider != null && isSelected == true && Input.GetMouseButtonDown(0) && cameraRay.transform.tag == "Plane")
+        if (cameraRay.collider != null && Input.GetMouseButtonDown(0) && cameraRay.transform.tag == "Player2")
         {
-            isActions = false;
-            isWhait = false;
-            isMove = true;
-            transform.position = cameraRay.transform.position;
+            p2isSelected = true;
+            p1isSelected = false;
+            player2.GetComponent<Renderer>().material = selectedMaterial;
+            player1.GetComponent<Renderer>().material = p1Material;
+        }
+
+        //else
+
+        if (cameraRay.collider != null && p1isSelected == true && Input.GetMouseButtonDown(0) && cameraRay.transform.tag == "Plane")
+        {
+            player1.transform.position = cameraRay.transform.position;          
+        }
+
+        if (cameraRay.collider != null && p2isSelected == true && Input.GetMouseButtonDown(0) && cameraRay.transform.tag == "Plane")
+        {
+            player2.transform.position = cameraRay.transform.position;
         }
     }
 }
