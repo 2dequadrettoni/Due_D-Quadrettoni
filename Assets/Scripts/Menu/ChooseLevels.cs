01d35550d9add9008410228dab02a4d02889c902
Andrea Viglietti <andrea.viglietti96@gmail.com>
2017-07-27 12:42:40 +0200

    Menù semicompleted

--- Assets/Scripts/Menu/ChooseLevels.cs
+++ Assets/Scripts/Menu/ChooseLevels.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChooseLevels : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    public GameObject MainMenu;
+    public GameObject LevelCanvas;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Space) && LevelCanvas.activeInHierarchy)
+        {
+            MainMenu.SetActive(true);
+            LevelCanvas.SetActive(false);
+        }
+
+    }
 
     public void Loadlevel(int index)
     {
 
         SceneManager.LoadScene(index);
     }
+
+
 }
