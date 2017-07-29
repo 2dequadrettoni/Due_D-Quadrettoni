1a14625dbd89d9d396a60759b3974fa6bc927a78
RobertoLeo <boris47@hotmail.it>
2017-07-29 16:26:24 +0200

    Classss for save and read saved level

---
+++ Assets/Scripts/MegaSaveReadLoadWriteClass.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.IO;
+
+public class MegaSaveReadLoadWriteClass {
+
+	const string SAVE_FILE_NAME = "lvl.asd";
+
+	public int GetSavedlevel() {
+
+		if ( !System.IO.File.Exists( SAVE_FILE_NAME ) ) return -1;
+
+		string value = File.ReadAllText( SAVE_FILE_NAME );
+
+		int iLevel = 0;
+
+		if ( !Int32.TryParse( value, out iLevel ) ) return -1;
+
+		return iLevel;
+
+
+	}
+
+	public void SaveLevel( int iCurrent_Level ) {
+		
+		File.WriteAllText( SAVE_FILE_NAME, iCurrent_Level.ToString() );
+
+	}
+
+}
