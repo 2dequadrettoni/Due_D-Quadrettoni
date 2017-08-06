eaa5ce2791dc20bf81726962b81b2d2ad69794d2
AndreaV96 <andrea.viglietti96@gmail.com>
2017-08-06 12:18:16 +0200

    aggiunta schermata di caricamento e corretti alcuni prefab

---
+++ Assets/Scripts/Menu/LoadingScreenManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenManager : MonoBehaviour {
+	
+	public GameObject loadingScreenObj;
+	AsyncOperation async;
+
+
+
+	public void LoadScreen(int i){
+	
+		StartCoroutine (LoadingScreen(i));
+	}
+
+	IEnumerator LoadingScreen(int index){
+
+		loadingScreenObj.SetActive (true);
+		async = SceneManager.LoadSceneAsync(index);
+		async.allowSceneActivation = false;
+
+		while (async.isDone == false) {
+
+			if (async.progress == 0.9f) {
+				async.allowSceneActivation = true;
+			}
+
+			yield return null;	
+		}
+
+	}
+}
+
